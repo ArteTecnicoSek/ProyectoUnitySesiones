dc62ccee3028c187ae830c4ddedd364e9a8d745d
Inner Child <contact@innerchild.cl>
2020-04-22 15:26:42 -0400

    primer push sesion 4

---
+++ Assets/TallerAssets/Scripts/SetWorldPositionToAllShaders.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetWorldPositionToAllShaders : MonoBehaviour
+{
+    [Tooltip("Nombre la propiedad Vector3 en el shader. NO debe estar expuesta")]
+    [Header("Nombre la propiedad Vector3 en el shader.NO debe estar expuesta")]
+
+    public string shaderVectorName;
+    [Tooltip("Si no colocas nada es el dueno del script")]
+    [Header("Si no colocas nada se asume que es el dueno del script")]
+    public Transform triggerTransform;
+
+
+
+    private void OnEnable()
+    {
+        if (triggerTransform == null)
+            triggerTransform = GetComponent<Transform>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector(shaderVectorName, triggerTransform.position);
+    }
+}
